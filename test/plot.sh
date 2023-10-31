4c9cf46f6dfeb2b9e4f46031c666c0c1f8bd1a3a
Meryem BOUHARRA <meryem.bouharra@telecom-sudparis.eu>
2023-10-31 16:33:03 +0100

    .

--- test/plot.sh
+++ test/plot.sh
@@ -7,12 +7,11 @@ step=500
 num_runs=5
 
 # Output files
-output_brute_force="execution_times_brute_force.dat"
-output_barnes_hut="execution_times_barnes_hut.dat"
+output="execution_timese.dat"
+"
 
-# Clear the existing output files
-> "$output_brute_force"
-> "$output_barnes_hut"
+
+> "$output"
 
 # Loop over different numbers of particles
 for ((nparticles = min_particles; nparticles <= max_particles; nparticles += step)); do
@@ -21,22 +20,33 @@ for ((nparticles = min_particles; nparticles <= max_particles; nparticles += ste
   # Run the nbody_brute_force program and collect execution time
   total_time_brute_force=0
   for ((i = 1; i <= num_runs; i++)); do
-    duration=$(../sequential/nbody_brute_force $nparticles | grep "Simulation took" | awk '{print $3}')
-    total_time_brute_force=$(echo "$total_time_brute_force + $duration" | bc)
-  done
-  average_time_brute_force=$(echo "scale=4; $total_time_brute_force / $num_runs" | bc)
+  #sequential
+  duration=$(../sequential/nbody_brute_force $nparticles | grep "Simulation took" | awk '{print $3}')
+  #duration=$(../sequential/nbody_barnes_hut $nparticles | grep "Simulation took" | awk '{print $3}')
 
-  # Run the nbody_barnes_hut program and collect execution time
-  total_time_barnes_hut=0
-  for ((i = 1; i <= num_runs; i++)); do
-    duration=$(../sequential/nbody_barnes_hut $nparticles | grep "Simulation took" | awk '{print $4}')
-    total_time_barnes_hut=$(echo "$total_time_barnes_hut + $duration" | bc)
+  #openMP
+  duration=$(../openMP/nbody_brute_force $nparticles $T_FINAL | grep "Simulation took" | awk '{print $3}')
+  #duration=$(../openMP/nbody_barnes_hut $nparticles $T_FINAL | grep "Simulation took" | awk '{print $3}')
+
+  #cuda
+  #duration=$(../cuda/nbody_brute_force $nparticles $T_FINAL | grep "Simulation took" | awk '{print $3}')
+  #duration=$(../cuda/nbody_barnes_hut $nparticles $T_FINAL | grep "Simulation took" | awk '{print $3}')
+
+  #mpi
+  #duration=$(mpirun -n 4 ... ../mpi/nbody_brute_force $nparticles $T_FINAL | grep "Simulation took" | awk '{print $3}')
+  #duration=$(mpirun -n 4 ... ../mpi/nbody_barnes_hut $nparticles $T_FINAL | grep "Simulation took" | awk '{print $3}')
+
+  #mpi-cuda
+  #duration=$(../mpi_cuda/nbody_brute_force $nparticles $T_FINAL | grep "Simulation took" | awk '{print $3}')
+  #duration=$(../mpi_cuda/nbody_barnes_hut $nparticles $T_FINAL | grep "Simulation took" | awk '{print $3}')
+
+    total_time=$(echo "$total_time + $duration" | bc)
   done
-  average_time_barnes_hut=$(echo "scale=4; $total_time_barnes_hut / $num_runs" | bc)
+  average_time=$(echo "scale=4; $total_time / $num_runs" | bc)
+  
 
   # Append the results to output files
-  echo "$nparticles $average_time_brute_force" >> "$output_brute_force"
-  echo "$nparticles $average_time_barnes_hut" >> "$output_barnes_hut"
+  echo "$nparticles $average_time" >> "$output"
 done
 
 # Plot the data using GNU Plot
@@ -46,8 +56,8 @@ set title "Number of Particles vs. Execution Time"
 set xlabel "Number of Particles"
 set ylabel "Average Execution Time (seconds)"
 set key right top
-plot "$output_brute_force" using 1:2 with lines title "Brute Force", \
-     "$output_barnes_hut" using 1:2 with lines title "Barnes-Hut"
+plot "$output" using 1:2 with lines title "Brute Force"
+
 pause -1 "Press Enter to close the plot window"
 EOL
 
