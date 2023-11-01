5bae1c5fafd94121b1112065b1aae30b855cc272
Guillermo Daniel Toyos Marfurt <guillermo.toyos@fing.edu.uy>
2023-11-02 00:26:16 +0100

    Update benchmark.py

--- test/benchmark.py
+++ test/benchmark.py
@@ -4,7 +4,13 @@ import subprocess
 
 NUM_OF_RUNS = 10
 
-for i in range(NUM_OF_RUNS):
-    output = subprocess.run("../sequential/nbody_brute_force $nparticles $T_FINAL | grep 'Simulation took' | awk '{print $3}'", shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
-    print(float(output))
-    
+sequential_results = []
+
+for p in range(100,8000,500):
+    s=0
+    for i in range(NUM_OF_RUNS):
+        output = subprocess.run("../sequential/nbody_brute_force $nparticles $T_FINAL | grep 'Simulation took' | awk '{print $3}'", shell=True,stdout=subprocess.PIPE).stdout.decode('utf-8')
+        s += float(output)
+    sequential_results.append((p,s/NUM_OF_RUNS))
+print(sequential_results)
+        
